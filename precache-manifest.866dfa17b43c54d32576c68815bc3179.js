e765d7623a4ec82653c8bed579f72716afe1a7a5
Gilmar Silva <gilmarheavensilva@hotmail.com>
2019-12-05 07:26:44 -0300

    bananinha

---
+++ precache-manifest.866dfa17b43c54d32576c68815bc3179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d854d6122e91a003003490f26c43dff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a84575f317f95001e24",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "2a84575f317f95001e24",
+    "url": "/static/js/2.7b085082.chunk.js"
+  },
+  {
+    "revision": "8213d64c931687f9dfb6",
+    "url": "/static/js/main.ea957eae.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
